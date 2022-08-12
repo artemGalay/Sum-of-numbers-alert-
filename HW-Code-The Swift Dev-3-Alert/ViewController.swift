84bc1ce79e3858fc539d16deda0d3e0c8ed39633
Artem Galay <iostema@mail.ru>
2022-08-12 12:58:25 +0300

    Initial Commit

---
+++ HW-Code-The Swift Dev-3-Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-Code-The Swift Dev-3-Alert
+//
+//  Created by Артем Галай on 12.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
